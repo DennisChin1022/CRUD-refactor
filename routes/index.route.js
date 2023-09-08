fe7d32b560b4e18cd18536a4d3b76a9f8d1fbb78
Dennis <dccy1022@outlook.com>
2023-09-08 09:08:41 +0800

    failed test

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,20 +3,33 @@ const Customer = require('../models/customer.model');
 const axios = require('axios');
 
 
-// router.get('/', (req, res, next) => {
-//   res.render('index');
-// });
+router.get('/createasset', (req, res, next) => {
+  res.render('createasset');
+});
 
-// router.get('/watchdata', async (req, res, next) => {
-//   try {
-//     const {serialnumber} = req.body;
-//     const serialnumbers = await axios.get('http://18.138.197.252:801/ORBT?requestType=getAlias&aliasName=' + serialnumber);
-//     const watchaccount = await axios.get('http://18.138.197.252:801/ORBT?requestType=getAsset&asset=' + serialnumbers.data.aliasURI);
-//     res.render('watchdata', { watchaccount });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+router.post('/createasset', async (req, res, next) => {
+  try {
+    if(true){
+    res.render('createasset', {
+      name: req.body.name,
+      description: req.body.description,
+      quantityQNT: req.body.quantityQNT,
+      decimals: req.body.decimals,
+      secretPhase: req.body.secretPhase,
+      feeNQT: req.body.feeNQT,
+      deadline: req.body.deadline,
+    });
+    console.log(req.body)
+  }
+  
+  const newassets = await axios.post('http://18.138.197.252:801/test?requestTag=AE')
+  const assets = await axios.get('http://18.138.197.252:801/ORBT?requestType=getAssetsByIssuer&account=ORBT-E8JD-FHKJ-CQ9H-5KGMQ&firstIndex=0&lastIndex=20')
+  console.log(assets)
+
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/', async (req, res, next) => {
   try {
@@ -68,34 +81,6 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const {serialnumber} = req.body;
-//     console.log(req.body)
-//     const serialnumbers = await axios.get('http://18.138.197.252:801/ORBT?requestType=getAlias&aliasName=' + serialnumber);
-//     console.log(serialnumbers.data)
-//     if (serialnumbers.data.aliasURI && serialnumbers.data.accountRS ) {
-//       // const {aliasuri} = serialnumbers.data.aliasURI;
-//       console.log(serialnumbers.data.aliasURI)
-//       console.log(serialnumbers.data.accountRS)
-//       const watchaccount = await axios.get('http://18.138.197.252:801/ORBT?requestType=getAsset&asset=' + serialnumbers.data.aliasURI);
-//       console.log(watchaccount.data)
-//       res.redirect('/watchdata');
-//     }else{
-//     req.flash('warning', 'Serial Number Not Valid');
-//     res.redirect('/');}
-    
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
-// axios.get('http://18.138.197.252:801/ORBT?requestType=getAlias&aliasName=rolex123456')
-// .then(result => {
-//   console.log(result.data.aliasName);
-// })
-// .catch(error => {
-//   console.log(error);
-// });
 
 module.exports = router;
